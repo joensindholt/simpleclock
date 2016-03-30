4ef43c41dc211a89d2c08ff63cf4ac0aba4ee0de
joensindholt <joensindholt@gmail.com>
2016-03-30 21:38:49 +0200

    24 hour clock and fontsize based on orientation

--- index.ios.js
+++ index.ios.js
@@ -4,6 +4,7 @@
  */
 
 var moment = require('moment');
+var daLocale = require('moment/locale/da');
 var IdleTimerManager = require('NativeModules').IdleTimerManager;
 
 import React, {
@@ -12,7 +13,8 @@ import React, {
     StyleSheet,
     Text,
     View,
-    StatusBar
+    StatusBar,
+    Dimensions
 } from 'react-native';
 
 class simpleClock extends Component {
@@ -20,34 +22,47 @@ class simpleClock extends Component {
     constructor() {
         super();
 
-        this.state = {};
+        this.state = {
+            fontSize: 140
+        };
     }
 
     componentDidMount() {
-        this.setState({clock: moment().format('hh:mm')});
+        // use danish local
+        moment.locale('da', daLocale);
+        // set the initial clock
+        this.setState({clock: moment().format('HH:mm')});
+        // update the clock every minute - yes, it can cause a minute to be skipped but what the heck
         setInterval(() => {
-            this.setState({clock: moment().format('hh:mm')});
+            this.setState({clock: moment().format('HH:mm')});
         }, 1000 * 60);
     }
 
     componentWillMount() {
+        // Force iPhone to stay on
         IdleTimerManager.setIdleTimerDisabled(true);
     }
 
     componentWillUnmount() {
+        // Allow iPhone to turn off again
         IdleTimerManager.setIdleTimerDisabled(false);
     }
 
     render() {
         return (
-            <View style={styles.container}>
+            <View style={styles.container} onLayout={this._onLayout}>
                 <StatusBar hidden={true}/>
-                <Text style={styles.clock}>
+                <Text style={[styles.clock, {fontSize: this.state.fontSize}]}>
                     {this.state.clock}
                 </Text>
             </View>
         );
     }
+
+    _onLayout = event => {
+        var {height, width} = event.nativeEvent.layout;
+        this.setState({'fontSize': width > 400 ? 140 : 100} );
+    };
 }
 
 const styles = StyleSheet.create({
@@ -58,7 +73,7 @@ const styles = StyleSheet.create({
         backgroundColor: 'black'
     },
     clock: {
-        fontSize: 140,
+        fontFamily: 'HelveticaNeue-Light',
         textAlign: 'center',
         margin: 10,
         color: '#333'
